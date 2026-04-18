fd2259465a4b99dd055f6258ae3abe715d58b396
gillianrhett <gillianrhett@gmail.com>
2026-04-17 18:06:22 -0700

    working on function to simulate one time period for one library

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -3,17 +3,19 @@
 #include <fstream>
 #include <map>
 #include <list>
+#include <array>
 #include <cstdlib>
 
 using namespace std;
 
 // Constants: Checkout_Base_Rate, Noreturn_Rate, Donate_Rate, Consumed_Base_Rate, ReplaceConsumables_Rate
-const double Checkout_Base_Rate = 15.0, Noreturn_Rate = 1.0, Donate_Rate = 1.0, Consumed_Base_Rate = 5.0, ReplaceConsumables_Rate = 5.0;
+const double Checkout_Base_Rate = 15.0, Return_Rate = 15.0, Donate_Rate = 1.0, Consumed_Base_Rate = 5.0, ReplaceConsumables_Rate = 5.0;
 
 // Define a function to simulate one week for one library
-    // Parameters: map&, library (key), checkout_rate_modifier, p_noreturn_modifier, p_donate_modifier, consumed_rate_modifier
+    // Parameters: map&, library (key), checkout_rate_modifier, return_rate_modifier, donate_rate_modifier, consumed_rate_modifier
     // use the size of the People list as a factor in these calculations
     // return value is void, it just adds and removes items from this library's lists
+void weekly_changes(map<string, array<list<string>,3>>&, string, double, double, double, double);
 
 // I might only need these functions for testing purposes
 // ---
@@ -29,6 +31,7 @@ const double Checkout_Base_Rate = 15.0, Noreturn_Rate = 1.0, Donate_Rate = 1.0, 
 
 // Define main function
 int main() {
+    srand(time(0));
     // Initialize a map to store the information about each of three libraries
 
     // Open an external file to read the book titles into each library - all libraries start with one copy of every book
@@ -44,14 +47,7 @@ int main() {
     // Begin a time-based simulation of a normal time period of the library activities
     // For 25 time intervals
     // Iterate through each library in the map
-	    // 1. books get checked out
-        // 2. books get returned or donated (this version will not keep track of which specific books 
-        //    were checked out; it will just randomly add books from the file)
-        // 3. consumables get used up
-        // 4. more consumables get bought
-        // 5. new people get library cards
-        // 6. people move away and are removed from the People list
-        // Print the changes for this interval
+
         // Wait or pause briefly to simulate the passage of time between intervals
 
     // Simulate what happens after the rate of new people joining increases
@@ -61,3 +57,21 @@ int main() {
     return 0;
 }        
 // End of main function
+
+void weekly_changes(map<string, array<list<string>,3>>& m, string library_name, double checkout_rate_modifier, double return_rate_modifier, double donate_rate_modifier, double consumed_rate_modifier) {
+// simulate the changes to books, consumables, and people for the given library in one week
+    int r = 0; // will store random numbers
+    // 1. books get checked out
+    int num_books = Checkout_Base_Rate * checkout_rate_modifier;
+    for (int i = 0; !m.empty() && i < num_books; ++i) {
+
+    }
+    // 2. books get returned or donated (this version will not keep track of which specific books 
+    //    were checked out; it will just randomly add books from the file)
+    num_books += (Return_Rate * return_rate_modifier) + (Donate_Rate * donate_rate_modifier);
+    // 3. consumables get used up
+    // 4. more consumables get bought
+    // 5. new people get library cards
+    // 6. people move away and are removed from the People list
+    // Print the changes for this interval
+}
